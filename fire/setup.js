6797e97a9ac43120d7b614359120bdc79d51a55a
Ashi Krishnan <me@ashi.works>
2018-01-10 16:37:13 -0500

    Firebase install and config.

--- fire/setup.js
+++ fire/setup.js
@@ -1,1 +1,14 @@
-throw new Error('fire/setup.js does not contain a config. You need to run `npm prepare`')
+module.exports = firebase =>
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+firebase.initializeApp({
+  "apiKey": "AIzaSyBVnPWHIRij4ffGA9jvmRSCTHUTFDcCoTU",
+  "databaseURL": "https://prism-82add.firebaseio.com",
+  "storageBucket": "prism-82add.appspot.com",
+  "authDomain": "prism-82add.firebaseapp.com",
+  "messagingSenderId": "199287280075",
+  "projectId": "prism-82add"
+});
+
